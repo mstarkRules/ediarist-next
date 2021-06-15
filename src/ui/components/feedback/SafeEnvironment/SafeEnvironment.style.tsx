602c470d3c7d81e8d6762abf0c8e72bc72812857
mstark-out <markstm10@gmail.com>
2021-06-15 18:50:17 -0300

    🛠 finish ambiente seguro componente

--- src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
+++ src/ui/components/feedback/SafeEnvironment/SafeEnvironment.style.tsx
@@ -1,11 +1,9 @@
-import { experimentalStyled as styled } from '@material-ui/core/styles';
+import { experimentalStyled as styled } from "@material-ui/core/styles";
 
-
-export const SafeEnvironmentContainer = styled('div')`
-    color: black;
-    text-align: right;
-    padding: 16px 0;
-    font-size: 12px;
-
+export const SafeEnvironmentContainer = styled("div")`
+  color: ${({ theme }) => theme.palette.text.secondary};
+  text-align: right;
+  padding: ${({ theme }) => theme.spacing(2)} 0;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.palette.background.default};
 `;
-
